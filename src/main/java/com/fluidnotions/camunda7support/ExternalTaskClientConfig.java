715ebe77251080606a660ad7a97c8b6fdd39dac2
Justin <justin.robinson@vcontractor.co.za>
2023-09-13 10:17:44 +0200

    fat jar wip

---
+++ src/main/java/com/fluidnotions/camunda7support/ExternalTaskClientConfig.java
@@ -0,0 +1,18 @@
+package com.fluidnotions.camunda7support;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExternalTaskClientConfig {
+
+    @Value("${camunda.bpm.client.base-url}")
+    private String basePath;
+
+    @Bean
+    public ExternalTaskClient taskClient() {
+        return ExternalTaskClient.create().baseUrl(basePath).asyncResponseTimeout(10000).build();
+    }
+}
